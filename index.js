bf907814fd64b8e68d0f7d58cddf83efa3eab7be
Ronaldo3030 <ronaldomacielcamposjunior@gmail.com>
2023-09-15 10:19:29 -0300

    :sparkles: feat: add try catch in main file

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
 const fs = require('fs')
 const { compileNode } = require('./utils')
 
-const fileName = process.argv[2]
-const file = fs.readFileSync(String(fileName), 'utf8')
+try {
+  const fileName = process.argv[2]
+  const file = fs.readFileSync(String(fileName), 'utf8')
+  const compile = (jsonCode) => {
+    const ast = JSON.parse(jsonCode)
 
-const compile = (jsonCode) => {
-  const ast = JSON.parse(jsonCode)
+    const code = compileNode(ast.expression);
+    // console.log(code)
+    eval(code)
+  }
 
-  const code = compileNode(ast.expression);
-  // console.log(code)
-
-  eval(code)
+  const start = process.hrtime();
+  compile(file);
+  const diff = process.hrtime(start);
+  const nanoseconds = diff[0] * 1e9 + diff[1];
+  const seconds = nanoseconds / 1e9;
+  console.log(`\x1b[34mInterpreter Exec. Time in: ${seconds.toFixed(2)} seconds\x1b[0m`);
+} catch (error) {
+  console.log('\x1b[31m ERROR: \n \x1b[0m' + error.message);
 }
 
-compile(file);
